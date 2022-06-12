3dc982db16127069fffa122c8f66f2b0566ad926
Ketan <ketanmangukiya001@gmail.com>
2022-06-12 22:10:54 +0530

    cors error

--- donation/settings.py
+++ donation/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-!s1o(*==*%$xy5%6qe51jil4tan2c15sm)=!q)tn&ux=q538k&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -39,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'app'
+    'app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
